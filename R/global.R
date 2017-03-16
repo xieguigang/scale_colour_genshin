913653d64411dd11a4aaa1f042db7c51adefe05c
谢桂纲 <xie.guigang@gcmodeller.org>
2017-03-16 08:30:20 +0800

    global configs

--- R/global.R
+++ R/global.R
@@ -7,4 +7,21 @@
 # Get current user's home directory
 HOME <<- path.expand('~');
 # and then combine this HOME directory for initialize this package's configuration data.
-cfg <- sprintf("%s/GCModeller.cli2R.csv", HOME);
+cfg <<- sprintf("%s/GCModeller.cli2R.csv", HOME);
+
+# 配置数据项目列表
+GCModeller.bin <<- NA;
+
+if(FileIO.FileLength(cfg) <= 0) {
+	warning("The GCModeller configuration have not been initialize yet! \nUnless you have already add the GCModeller/bin path into your system's environment variable... \nPlease run 'GCModeller.cfg' function for configs this scripting environment...");
+} else {
+
+	# 配置文件存在，读取csv文件，然后赋值给全局的环境变量
+	configs <- read.csv(cfg);
+	
+}
+
+# 将配置数据通过生成一个dataframe对象然后写入csv文件之中作为配置数据
+GCModeller.cfg <- function(bin) {
+	
+}
