5c4c5730318d2bc2e7afc09dd066fce19573ce5b
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-13 16:51:15 +0800

    improvements on the debug output

--- GCModeller.cli2R/R/PI.R
+++ GCModeller.cli2R/R/PI.R
@@ -6,12 +6,22 @@ options(stringsAsFactors = FALSE)
 PI.plot <- function(csv) {
 
 	raw<-read.csv(file=csv,header=TRUE)
+
 	DIR <- dirname(csv)
 	DIR <- paste(DIR, file_path_sans_ext(basename(csv)), sep="/")
 
 	tiff(paste(DIR, "pimw.tiff", sep="-"),width=2500,height=2000,res=400)
-	plot(raw[,"calc..pI"],raw[,"MW..kDa."],xlab="Calc.PI",ylab="MW[Kda]",pch=15)
-	dev.off()
-
+	
+		names <- colnames(raw);
+		pI <- "calc..pI";
+		MW <- "MW..kDa.";
+		
+		if ((!(pI %in% names)) || (!(MW %in% names))) {
+			print(names);
+		}
+		
+		plot(raw[, pI],raw[, MW],xlab="Calc.PI",ylab="MW[Kda]",pch=15)
+		
+	dev.off();
 }
 
