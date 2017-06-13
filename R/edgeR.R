cfad2d60befb2c7362804efc73377554d7927f4a
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-13 12:20:44 +0800

    move directory

--- R/edgeR.R
+++ R/edgeR.R
@@ -17,8 +17,9 @@ run.edgeR <- function(table) {
 	
 	# 分组变量 前两个为一组， 后一个为一组， 每个有repeats个重复
 	group   <- factor(append(x, y));
+	range   <- 2:(ncol(table));
 	
-	y     <- DGEList(counts=table, group=group) # 构建基因表达列表
+	y     <- DGEList(counts=table[, range], group=group) # 构建基因表达列表
 	y     <- calcNormFactors(y) # 计算样本内标准化因子
 	y     <- estimateCommonDisp(y) #计算普通的离散度
 	y     <- estimateTagwiseDisp(y) #计算基因间范围内的离散度
