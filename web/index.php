ed3db35ace978e1f2b02926c3d3da292d1418441
xieguigang <xie.guigang@gcmodeller.org>
2020-01-15 22:53:23 +0800

    login page

--- web/index.php
+++ web/index.php
@@ -24,7 +24,7 @@ class App {
     public function login() {
         if (pwa::login_userId() < 0) {
             # 未登录状态
-            View::Display();
+            View::Display(["hide" => "display: none;"]);
         } else {
             Redirect("/home");
         }
