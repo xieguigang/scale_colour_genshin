43a8d689fd5e023a92a76fcb70f3ea991f3ed05e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-01-15 12:24:26 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,16 +6,24 @@ class App {
     
     /**
      * @uses view
+     * @access *
     */
     public function index() {
         View::Display();
     }
 
 	/**
+     * @uses view
+    */
+    public function gallery() {
+    	View::Display();
+    }
+	
+	/**
 	 * @uses api
 	 * @access *
 	*/
     public function update() {
 		
-	}
+    }
 }
