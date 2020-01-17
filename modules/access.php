f818b04ae4516171629c84cdb1f064a5ce94ddbc
xieguigang <xie.guigang@gcmodeller.org>
2020-01-17 22:06:20 +0800

    fix for the page view logging event bugs

--- modules/access.php
+++ modules/access.php
@@ -16,30 +16,31 @@ class accessController extends controller {
         ]);
     }
 
-    public function accessControl() {
-        $access = $this->getAccessLevel();
-        $type   = $this->getUsage();
+    private function log_pageView($access) {
+        $type = $this->getUsage();
 
         # 从这里统计用户的活动信息
         if ($type == "view" || $type == "router") {
             // 只针对html页面的访问进行统计
-            self::recordEvent(200);
+            self::recordEvent($access ? 200 : 403);
         }
 
+        return $access;
+    }
+
+    public function accessControl() {
+        $access = $this->getAccessLevel();        
+
         if ($this->AccessByEveryOne()) {
-            return true;
+            return $this->log_pageView(true);
         } else if (empty($access)) {
             // 什么也没有填写的时候，默认为登录用户才可以访问
-            return pakchoi::login_userId() > 0;
+            return $this->log_pageView(pakchoi::login_userId() > 0);
         } else {
             // access现在不为空的了
             $access = explode("|", $access);
         } 
         
-        // if (in_array("wechat", $access) && $this->isWeChatAccess()) {
-        //     return true;
-        // }
-
         // 例如控制器被标记为
         // @access wechat|admin
         // 表示只能够通过微信端或者具有管理员权限的用户才可以访问当前的这个控制器
@@ -49,17 +50,27 @@ class accessController extends controller {
         // 表示只能够具有管理员权限的用户才可以访问当前的这个控制器
         //
         // 则权限管理可以通过下面的循环来实现
-        // $roles = self::getUserRoleEnums();
+        $roles = self::getUserRoleEnums();
 
-        // foreach ($access as $role) {
-        //     if (array_key_exists($role, $roles)) {
-        //         if (self::isRole($roles[$role])) {
-        //             return true;
-        //         }
-        //     }
-        // }
+        foreach ($access as $role) {
+            if (array_key_exists($role, $roles)) {
+                if (self::isRole($roles[$role])) {
+                    return $this->log_pageView(true);
+                }
+            }
+        }
         
         // 当前的用户任何权限条件都不满足，则禁止访问当前的这个控制器
+        return $this->log_pageView(false);
+    }
+
+    private static function isRole($roleId) {
         return false;
+    }
+
+    private static function getUserRoleEnums() {
+        return [
+            "admin" => 999
+        ];
     }
 }
