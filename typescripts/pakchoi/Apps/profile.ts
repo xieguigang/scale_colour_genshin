71e0c696612e41026d53e4341fc89e7a74d2958c
xieguigang <xie.guigang@gcmodeller.org>
2020-01-24 00:02:30 +0800

    tweaks of the ui display

--- typescripts/pakchoi/Apps/profile.ts
+++ typescripts/pakchoi/Apps/profile.ts
@@ -23,26 +23,30 @@ namespace pages {
 
         private renderLoginList(logins: loginActivity[]) {
             let list = $ts("#login-list");
+            let rowView: any;
 
             for (let login of logins) {
-                list
-                    .append($ts("<span>", {
-                        style: "font-size:0.8em;color:lightgray"
-                    }).display(login.create_time))
-                    .append($ts("<span>", {
-                    }).display(login.content));
+                rowView = $ts("<span>", {
+                    style: "font-size:0.8em;color:lightgray"
+                }).display(login.create_time).append($ts("<span>", {
+                }).display(login.content));
+
+                list.append($ts("<li>").display(rowView));
             }
         }
 
         private renderVisitList(visits: pageViewActivity[]) {
             let list = $ts("#visit-list");
+            let rowView: any;
 
             for (let visit of visits) {
-                list.append($ts("<span>", {
+                rowView = $ts("<span>", {
                     style: "font-size:0.8em;color:lightgray"
-                }).display(visit.time))
+                }).display(visit.time)
                     .append($ts("<span>", {
-                    }).display($ts("<a>", { href: visit.page_url }).display(visit.page_url)))
+                    }).display($ts("<a>", { href: visit.page_url }).display(visit.page_url)));
+
+                list.append($ts("<li>").display(rowView));
             }
         }
     }
