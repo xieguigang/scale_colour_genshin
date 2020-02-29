f3eb708ed10e2a5ae8dba6ed2fb85a2cf1086982
xieguigang <xie.guigang@gcmodeller.org>
2020-02-29 11:13:48 +0800

    profile api

--- web/api.php
+++ web/api.php
@@ -17,32 +17,6 @@ class App {
     }
 
     /**
-     * @uses api
-    */
-    public function latest_visits() {
-        $visits = (new Table("pageview"))
-        ->where(["user_id" => $_SESSION["id"], "code" => 200])
-        ->limit(10)
-        ->order_by("time", true)
-        ->select();
-
-        controller::success($visits);
-    }
-
-    /**
-     * @uses api
-    */
-    public function latest_logins() {
-        $logins = (new Table("activity"))
-        ->where(["user" => $_SESSION["id"], "type" => 0])
-        ->limit(10)
-        ->order_by("create_time", true)
-        ->select();
-
-        controller::success($logins);
-    }
-
-    /**
      * 分享当前的位置
      * 
      * @uses api
