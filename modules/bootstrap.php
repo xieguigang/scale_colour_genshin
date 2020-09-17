824f55a54a025f0943a431be9ec8d269f1958d37
xieguigang <xie.guigang@gcmodeller.org>
2020-09-18 00:19:26 +0800

    config web server

--- modules/bootstrap.php
+++ modules/bootstrap.php
@@ -2,6 +2,11 @@
 
 ini_set('session.cookie_lifetime',"86400‬");
 ini_set('session.gc_maxlifetime', "86400‬"); 
+ini_set('memory_limit', "1024M");
+ini_set('max_input_time', 3600);
+ini_set('max_execution_time', 3600);
+ini_set('post_max_size', "4096M");
+ini_set('upload_max_filesize', "4096M");
 
 session_start();
 
