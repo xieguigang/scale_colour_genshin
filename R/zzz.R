9311c8d4dd539f9f20004b6c55219b046508fafa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-03 13:35:27 +0800

    Update zzz.R
    register the custom color palette

--- R/zzz.R
+++ R/zzz.R
@@ -9,5 +9,15 @@ const .onLoad = function() {
     |> JSON::json_decode()
     ;
 
+    #' use color palette for heatmap/ms-imaging/etc
+    #' 
+    #' 1. use the character name directly, example as: "albedo"
+    #' 2. use the character name with genshin prefix, example as: "genshin:albedo"
+    #'
+    for(name in names(palette)) {
+        grDevices::register.color_palette(name, colors = palette[[name]]);
+        grDevices::register.color_palette(`genshin:${name}`, colors = palette[[name]]);
+    }
+    
     set(env, "scale_colours@genshin", palette);
 }
