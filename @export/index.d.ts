d75e0896b4b5c01160f657ce7b251bd8c495d57e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-12 01:56:40 +0800

    update runtime to .net 8.0

---
+++ @export/index.d.ts
@@ -0,0 +1,25 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=scale_colour_genshin
+
+declare namespace scale_colour_genshin {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+   */
+   function __get_inmemory_cache(): object;
+   /**
+     * @param name default value Is ``albedo``.
+   */
+   function character_colorSet(name?: any): object;
+   /**
+   */
+   function keys(): object;
+   /**
+     * @param name default value Is ``Call scale_colour_genshin::"keys"()``.
+   */
+   function scale_colour_genshin(name?: any): object;
+}
