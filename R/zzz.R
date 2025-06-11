901a3dbaf269f83f694c61490d8b04506fd9a3b4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-12 02:05:42 +0800

    fix of the symbol reference error

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,13 @@ require(JSON);
 const .onLoad = function() {
     const env = globalenv();
     const palette = "data/scale_colour_genshin.json" 
-    |> system.files(package = "scale_colour_genshin")
+    |> system.file(package = "scale_colour_genshin")
     |> readText()
     |> JSON::json_decode()
     ;
+
+    print("load genshin names:");
+    print(names(palette));
 
     #' use color palette for heatmap/ms-imaging/etc
     #' 
@@ -15,8 +18,8 @@ const .onLoad = function() {
     #' 2. use the character name with genshin prefix, example as: "genshin:albedo"
     #'
     for(name in names(palette)) {
-        grDevices::register.color_palette(name, colors = palette[[name]]);
-        grDevices::register.color_palette(`genshin:${name}`, colors = palette[[name]]);
+        grDevices::register.color_palette(name, palette = palette[[name]]);
+        grDevices::register.color_palette(`genshin:${name}`, palette = palette[[name]]);
     }
     
     set(env, "scale_colours@genshin", palette);
